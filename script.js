52c9191534c303ba9fe550e3ec80a9067d3835c9
GopalRatawal2010 <gopalratawal41@gmail.com>
2026-06-09 20:45:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById("btn").addEventListener("click", function() {
     alert("Welcome to my website!");
 });
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+card.addEventListener("click",()=>{
+card.classList.toggle("flip");
+});
+});
+
+// Smooth button animation
+
+document.querySelector("button").addEventListener("click",()=>{
+document.getElementById("skills").scrollIntoView({
+behavior:"smooth"
+});
+});
