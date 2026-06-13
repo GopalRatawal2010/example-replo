91fb7a46698d6e7bd2c5f8a52e278f65782d2400
GopalRatawal2010 <gopalratawal41@gmail.com>
2026-06-13 20:55:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -473,3 +473,23 @@ setTimeout(update,20);
 update();
 
 });
+
+const btn=document.getElementById("top");
+
+window.addEventListener("scroll",()=>{
+
+btn.style.display=window.scrollY>300?"block":"none";
+
+});
+
+btn.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
