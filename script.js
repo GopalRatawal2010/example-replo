6cd1e66eaf85b8e612f9834c856603a80889353b
GopalRatawal2010 <gopalratawal41@gmail.com>
2026-06-22 20:23:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -543,3 +543,35 @@ card.style.transform=
 });
 
 });
+const sections=document.querySelectorAll("section");
+const navLinks=document.querySelectorAll("nav a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(sec=>{
+
+const top=sec.offsetTop-200;
+
+if(scrollY>=top){
+
+current=sec.id;
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.href.includes(current)){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
